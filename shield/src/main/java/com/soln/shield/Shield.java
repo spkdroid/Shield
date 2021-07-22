a0ce2b2b419e624e790d092afb4b474aa613dce8
Ramkumar Velmurugan <vrk1990@outlook.com>
2021-07-21 22:02:30 -0400

    Detect Emulator code implemented

--- shield/src/main/java/com/soln/shield/Shield.java
+++ shield/src/main/java/com/soln/shield/Shield.java
@@ -3,7 +3,6 @@ package com.soln.shield;
 import android.content.Context;
 import android.os.Build;
 import android.provider.Settings;
-import android.widget.Toast;
 
 public class Shield {
 
@@ -20,6 +19,23 @@ public class Shield {
         return false;
     }
 
-
+    public static boolean isEmulator() {
+        return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+                || Build.FINGERPRINT.startsWith("generic")
+                || Build.FINGERPRINT.startsWith("unknown")
+                || Build.HARDWARE.contains("goldfish")
+                || Build.HARDWARE.contains("ranchu")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android SDK built for x86")
+                || Build.MANUFACTURER.contains("Genymotion")
+                || Build.PRODUCT.contains("sdk_google")
+                || Build.PRODUCT.contains("google_sdk")
+                || Build.PRODUCT.contains("sdk")
+                || Build.PRODUCT.contains("sdk_x86")
+                || Build.PRODUCT.contains("vbox86p")
+                || Build.PRODUCT.contains("emulator")
+                || Build.PRODUCT.contains("simulator");
+    }
 
 }
