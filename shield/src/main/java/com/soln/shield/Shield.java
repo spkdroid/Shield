1d875ba6e42a3666c7dec582fb232d165267edb1
Ramkumar Velmurugan <vrk1990@outlook.com>
2021-07-21 22:03:15 -0400

    code reformat

--- shield/src/main/java/com/soln/shield/Shield.java
+++ shield/src/main/java/com/soln/shield/Shield.java
@@ -7,15 +7,15 @@ import android.provider.Settings;
 public class Shield {
 
     public static boolean isDeveloperModeOn(Context c) {
-            if(Integer.parseInt(Build.VERSION.SDK) == 16) {
+        if (Integer.parseInt(Build.VERSION.SDK) == 16) {
+            return Settings.Secure.getInt(c.getContentResolver(),
+                    Settings.Secure.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0;
+        } else if (Integer.parseInt(Build.VERSION.SDK) >= 17) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
                 return Settings.Secure.getInt(c.getContentResolver(),
-                        Settings.Secure.DEVELOPMENT_SETTINGS_ENABLED , 0) != 0;
-            } else if (Integer.parseInt(Build.VERSION.SDK) >= 17) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    return Settings.Secure.getInt(c.getContentResolver(),
-                            Settings.Global.DEVELOPMENT_SETTINGS_ENABLED , 0) != 0;
-                }
+                        Settings.Global.DEVELOPMENT_SETTINGS_ENABLED, 0) != 0;
             }
+        }
         return false;
     }
 
@@ -37,5 +37,4 @@ public class Shield {
                 || Build.PRODUCT.contains("emulator")
                 || Build.PRODUCT.contains("simulator");
     }
-
 }
